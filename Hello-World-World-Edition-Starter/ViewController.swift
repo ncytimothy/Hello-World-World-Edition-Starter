630befcb4f6389d42713356050529eefe46b7521
Timothy Ng <timothyng2008@me.com>
2018-10-29 20:53:07 +0800

    - feat: Framework added with TODOs

--- Hello-World-World-Edition-Starter/ViewController.swift
+++ Hello-World-World-Edition-Starter/ViewController.swift
@@ -2,19 +2,74 @@
 //  ViewController.swift
 //  Hello-World-World-Edition-Starter
 //
-//  Created by Tim Ng on 10/29/18.
-//  Copyright © 2018 Tim Ng. All rights reserved.
+//  Learn Swift HK
+//
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//----------------------------------------------------------------------------------
+// MARK:- Properties
+    
+    var greetings = ["Welcome",
+                     "Velkommen",
+                     "Welkom",
+                     "歡迎",
+                     "Bienvenue",
+                     "Willkommen",
+                     "ようこそ",
+                     "Benvenuto",
+                     "Välkommen",
+                     "환영하다",
+                     "Bienvenido",
+                     "欢迎",
+                     "Tervetuloa",
+                     "Bem-vindo",
+                     "Добро пожаловать"
+    ]
+    
+    var pressMes = ["Press Me",
+                    "Trykk på Me",
+                    "Druk op mij",
+                    "按我",
+                    "Appuyez-moi",
+                    "Drück mich",
+                    "私を押す",
+                    "Premi Me",
+                    "Tryck på Me",
+                    "나를 압박해라",
+                    "Presionarme",
+                    "按我",
+                    "Paina Me",
+                    "Pressione-me",
+                    "Нажмите Me"
+    ]
+    
+//----------------------------------------------------------------------------------
+// MARK:- Outlets
+    // TODO: Add AutoLayout Constraints for Welcome Label and Press Me button in Main.Storyboard
+    // TODO: Create Welcome Label and Press Me button in Main.Storyboard
+    // TODO: Add outlets for Welcome Label and Press Me button
+   
+ 
+    
+//----------------------------------------------------------------------------------
+// MARK:- Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+//----------------------------------------------------------------------------------
+// MARK:- Actions
+    // TODO: Add IBAction for Press Button, randomizing the greetings
+    // IBAction should trigger: 1) Welcome Label text change, 2) Press Me Button Title Change
+    
+    
+    //----------------------------------------------------------------------------------
 
 }
 
